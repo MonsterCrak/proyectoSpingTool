68c491508c2140a3c7d8b10fd2e5b4ba1a0af0bb
joseguevara18 <joseguevara0918@gmail.com>
2023-06-11 00:48:11 -0500

    Se agrego entidades, repositorio y servicio para cliente

---
+++ DawiproyT5dn/src/main/java/com/dawiproy/repository/Clienterepository.java
@@ -0,0 +1,9 @@
+package com.dawiproy.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dawiproy.entity.Cliente;
+
+public interface Clienterepository extends JpaRepository<Cliente, Integer>{
+
+}
---
+++ DawiproyT5dn/src/main/java/com/dawiproy/services/Clienteservices.java
@@ -0,0 +1,35 @@
+package com.dawiproy.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dawiproy.entity.Cliente;
+import com.dawiproy.repository.Clienterepository;
+
+@Service
+public class Clienteservices {
+	@Autowired
+	private Clienterepository repo;
+	
+	public void registrar(Cliente bean) {
+		repo.save(bean);
+	}
+	
+	public void actualizar(Cliente bean) {
+        repo.save(bean);
+    }
+	
+	public Cliente buscarPorId(Integer cod) {
+	     return repo.findById(cod).orElse(null);
+	}
+	
+	public void eliminarPorID(Integer cod) {
+        repo.deleteById(cod);
+    }
+	
+	public List<Cliente> listarTodos(){
+		return repo.findAll();
+	}
+}

3564124568a30a8d382b45badad09c0ce82edecc
Jhoset Llacchua <jhosetsales@gmail.com>
2023-06-10 22:48:36 -0500

    Proyecto base

---
+++ DawiproyT5dn/src/main/java/com/dawiproy/DawiproyT5dnApplication.java
@@ -0,0 +1,13 @@
+package com.dawiproy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiproyT5dnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiproyT5dnApplication.class, args);
+	}
+
+}
---
+++ DawiproyT5dn/src/test/java/com/dawiproy/DawiproyT5dnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dawiproy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiproyT5dnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
